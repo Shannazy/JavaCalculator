271584c332a4cb857e6846658e52a45f68fe5857
Shannazy <shaziislam417@gmail.com>
2019-08-27 20:42:48 -0400

    Make stage and first page

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -4,17 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
 
     @Override
     public void start(Stage Calculator) throws Exception{
+        //Page Set up
         Calculator.setTitle("Calculations");
         Calculator.setHeight(600);
         Calculator.setWidth(400);
+        Calculator.setFullScreen(true);
         Calculator.show();
+
+        //Scene setup
+        BorderPane starter = new BorderPane(new Label("This the start page"));
+        Scene mainPage =  new Scene( starter);
+        Calculator.setScene(mainPage);
     }
 
 
